32cad40bcc5c6fd589edbe7e4a1964530f732fa4
Tim Jacomb <tim.jacomb@hmcts.net>
2019-03-01 17:22:28 +0000

    Initial version

--- docker/database/init-db-v9.6.sh
+++ docker/database/init-db-v9.6.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for service in pdns; do
+for service in pdns pdns_ui; do
 psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=$service --set PASSWORD=$service --set DATABASE=$service <<-EOSQL
   CREATE USER :USERNAME WITH PASSWORD ':PASSWORD';
   CREATE DATABASE :DATABASE WITH OWNER = :USERNAME ENCODING = 'UTF-8' CONNECTION LIMIT = -1;
